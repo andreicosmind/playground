9a5e707d548a49987cd52f173b9261ee2d38663f
UbbeRagnarsson <andreicosmind@yahoo.com>
2020-02-21 14:41:11 +0200

    new

--- Dice_Rolling_Simulator .py
+++ Dice_Rolling_Simulator .py
@@ -1,6 +1,15 @@
-print('hello')
+import random
 
 
-def lof():
-    pass
+def roll_dice():
+    print ('You rolled: ', random.randint(1, 6))
 
+def options():
+    a = input('Play or quit the game:\n\nPres p for play or q to exit the game: ')
+    if a == 'p':
+        roll_dice()
+        options()
+    else: exit()
+
+if __name__ == '__main__':
+    options()
