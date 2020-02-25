95401ac32f7ffa0736a10e93ab9ea9186465d769
iCe <andreicosmind@yahoo.com>
2020-02-25 02:48:49 +0200

    1

--- Dice_Rolling_Simulator .py
+++ Dice_Rolling_Simulator .py
@@ -2,14 +2,17 @@ import random
 
 
 def roll_dice():
-    print ('You rolled: ', random.randint(1, 6))
+    print('You rolled: ', random.randint(1, 6))
+
 
 def options():
-    a = input('Play or quit the game:\n\nPres p for play or q to exit the game: ')
+    a = input('Play or quit the game:\n\nPres p for play or q to exit the game: \n')
     if a == 'p':
         roll_dice()
         options()
-    else: exit()
+    else:
+        exit()
+
 
 if __name__ == '__main__':
     options()
